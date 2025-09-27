7935195fd3bd00e4946ca08916146a17ad5995a0
Xyryllium <sigh.real.xa@gmail.com>
2025-09-27 15:16:42 +0800

    Fix vercel serverless function config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-import { createRequestHandler } from "@react-router/node";
+import { createRequestListener } from "@react-router/node";
 import * as build from "../build/server/index.js";
 
-const handler = createRequestHandler(build, process.env.NODE_ENV);
+const handler = createRequestListener(build, process.env.NODE_ENV);
 
 export default handler;
